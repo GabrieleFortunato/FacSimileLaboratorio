a6c342bd9ebfe3d5bebeeea2d5d3645ed05eda19
gabriele <gabriele@192.168.1.100>
2017-06-17 09:14:00 +0200

    Effettuo il commit

--- src/eccezioni/PrezzoNegativoEccezione.java
+++ src/eccezioni/PrezzoNegativoEccezione.java
@@ -18,7 +18,7 @@ public class PrezzoNegativoEccezione extends Exception {
 	 * @param message
 	 */
 	public void printMessage(String message){
-		Logger.getLogger(message);
+		Logger.getLoger(message);
 	}
 	
 }
